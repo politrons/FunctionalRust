cb9279d2f320985c58f2d0c0abf63f7fc61fa8db
politrons <politrons@gmail.com>
2023-04-24 20:28:34 +0200

    extension_methods.rs

--- src/extension_methods.rs
+++ src/extension_methods.rs
@@ -1,6 +1,7 @@
 pub fn run() {
     let string_value = "Hello world".to_string();
     println!("Contains hello:{}",string_value.contains_hello());
+    println!("Number:{}",1981.multiply_by(10));
 }
 
 trait StringExt {
@@ -10,5 +11,16 @@ trait StringExt {
 impl StringExt for String {
     fn contains_hello(&self) -> bool {
         self.contains("Hello")
+    }
+}
+
+trait NumberExt {
+    fn multiply_by(&self,number:i32) -> i32;
+}
+
+impl NumberExt for i32 {
+
+    fn multiply_by(&self, number: i32) -> i32 {
+        self * number
     }
 }
