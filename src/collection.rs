9c246775509868da16381c42bf7706bb3870bdaf
Pablo Pérez García <politrons@gmail.com>
2023-04-27 09:37:52 +0200

    Functions)

--- src/collection.rs
+++ src/collection.rs
@@ -1,12 +1,18 @@
+use std::collections::HashMap;
+
 pub fn run() {
     array();
     vector();
     list();
     fold_list();
-    flat_map__list();
+    flat_map_list();
+    immutable_map_collection();
+    mutable_map_collection();
 }
 
-
+/**
+Superpower array type which can allow you to map, get, contains and other operators.
+*/
 fn array() {
     let list = [1, 2, 3, 4, 5]
         .map(|v| v + 10);
@@ -34,7 +40,10 @@ fn list() {
     println!("{}", result)
 }
 
-fn flat_map__list() {
+/**
+Iterator is also a Monad in Rust, so you can compose two iterators using [flat_map] operator
+*/
+fn flat_map_list() {
     let result = ["hello", "functional", "rust", "world", ]
         .into_iter()
         .flat_map(|e| [e.to_string() + &"!"].into_iter())
@@ -46,10 +55,33 @@ fn flat_map__list() {
 Fold operator is able just like in any other functional language, define an initial value type as first argument,
 and then pass a bi-function with the accumulative value in the specific type we made before, and the new element
 is on the collection.
-*/
+ */
 fn fold_list() {
     let result = ["hello", "functional", "rust", "world", ]
         .into_iter()
         .fold("-->".to_string(), |acc, elem| acc.to_string() + &"-".to_string() + &elem);
     println!("{}", result)
+}
+
+/**
+immutable map is bu design the default option when you create in rust all data types. Here there is no different.
+A map it can also be converter in iterable using [into_iter] operator
+*/
+fn immutable_map_collection() {
+    let map = HashMap::from([(1, "hello"), (2, "rust"), (3, "map")]);
+    map.into_iter()
+        .for_each(|(k, v)| println!("Key:{} Value:{}", k, v))
+}
+
+/**
+In case you need a mutable map to add/delete records on runtime, as usual you need to use [mut], and then
+you can use [insert] or [remove] operators
+*/
+fn mutable_map_collection() {
+    let mut map = HashMap::new();
+    map.insert(1, "hello");
+    map.insert(2, "mutable");
+    map.insert(3, "map");
+    map.into_iter()
+        .for_each(|(k, v)| println!("Key:{} Value:{}", k, v))
 }
