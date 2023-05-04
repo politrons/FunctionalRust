bf68f4e825be621e4fa690c7c9ccce7d5a8f08db
Pablo Pérez García <politrons@gmail.com>
2023-05-04 10:42:24 +0200

    Actix

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -3,7 +3,7 @@ mod actix_web_server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-   actix_server::run_server().await
-    // actix_web_server::run_server().await
+   // actix_server::run_server().await
+    actix_web_server::run_server().await
 
 }
