9c596dc6ead4103bd1c6e1735264b64900bdfa38
Pablo Pérez García <politrons@gmail.com>
2023-05-06 00:16:22 +0200

    Either monad

--- tokio/src/tokio_select.rs
+++ tokio/src/tokio_select.rs
@@ -7,6 +7,9 @@ In case we want to run several task in parallel and make a [race] like in [ZIO] 
 we can use [select!] which it will be subscribe to all futures in progress, and once it detects that
 one finish, it will cancel the rest of the futures in progress.
 
+[select!] it work with [tokio] channels, so it subscribe to all [receivers] each of them associated with\
+as callback operation. And once the received [receive] the data, it invoke the callback
+
 Here we emulate a Race between cars where once [select!] detect the first car in finish the race,
 we automatically invoke the callback associated, and it cancel the rest of futures.
 */
