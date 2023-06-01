0e01e5896071b9d1d61d806dcc3f0da497f1217e
Pablo Pérez García <politrons@gmail.com>
2023-06-01 13:47:12 +0200

    Red Panda

--- red_panda/src/red_panda_consumer.rs
+++ red_panda/src/red_panda_consumer.rs
@@ -1,20 +1,19 @@
 use rdkafka::client::ClientContext;
 use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
-use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
 use rdkafka::error::KafkaResult;
-use rdkafka::message::{Headers, Message};
+use rdkafka::message::{BorrowedMessage, Headers, Message};
 use rdkafka::topic_partition_list::TopicPartitionList;
 use rdkafka::util::get_rdkafka_version;
 
-
-// A context can be used to change the behavior of producers and consumers by adding callbacks
-// that will be executed by librdkafka.
-// This particular context sets up custom callbacks to log rebalancing events.
+/// Struct type to override [ClientContext], and override the callback functions.
 struct CustomContext;
 
 impl ClientContext for CustomContext {}
 
+
+///Callbacks to receive information when a consumer is in rebalance process, and every time it commit an offset.
 impl ConsumerContext for CustomContext {
     fn pre_rebalance(&self, rebalance: &Rebalance) {
         println!("Pre rebalance {:?}", rebalance);
@@ -24,18 +23,25 @@ impl ConsumerContext for CustomContext {
         println!("Post rebalance {:?}", rebalance);
     }
 
-    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
-        println!("Committing offsets: {:?}", result);
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) { println!("Committing offsets: {:?}", result);
     }
 }
 
-// A type alias with your custom consumer can be created for convenience.
-type LoggingConsumer = StreamConsumer<CustomContext>;
+async fn consume(brokers: &str, group_id: &str, topics: &[&str]) {
+    let consumer: StreamConsumer<CustomContext> = create_consumer(brokers, group_id, CustomContext);
+    consumer.subscribe(&topics.to_vec())
+        .expect("Can't subscribe to specified topics");
+    consume_records(consumer).await;
+}
 
-async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
-    let context = CustomContext;
-
-    let consumer: LoggingConsumer = ClientConfig::new()
+/// Creation of Consumer using [ClientConfig::new()] builder.
+/// We set the broker to connect, groupId, session timeout, and if we want to do auto commit.
+/// We use [create_with_context] to being able to create an pass our own implementation of the [ConsumerContext],
+/// that we already override some methods.
+/// We use [expect] to unwrap the [KafkaResult] obtained from the create operator, and we try to get the Ok,
+/// otherwise we print the error messages passed, and we have a panic.
+fn create_consumer(brokers: &str, group_id: &str, context: CustomContext) -> StreamConsumer<CustomContext> {
+    ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", brokers)
         .set("enable.partition.eof", "false")
@@ -43,17 +49,21 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
         .set("enable.auto.commit", "true")
         .set_log_level(RDKafkaLogLevel::Debug)
         .create_with_context(context)
-        .expect("Consumer creation failed");
+        .expect("Red panda Consumer creation failed")
+}
 
-    consumer
-        .subscribe(&topics.to_vec())
-        .expect("Can't subscribe to specified topics");
-
+///Using the consumer we invoke [recv] to block the consumer until it get a record from the broker.
+/// Since this operation is async we await, and once is resolved, we obtain a [Result]
+/// We pattern matching the Result to extract a [KafkaError] and continue, or get a [BorrowedMessage]
+/// With the we use [payload_view] to transform byte array into the type specify in the method.
+/// This will return an [Option] that we match to control side-effect of nullable.
+/// For this example we dont use the payload returned, that's why we define a [let _] in the match
+async fn consume_records(consumer: StreamConsumer<CustomContext>) {
     loop {
         match consumer.recv().await {
-            Err(e) => println!("Kafka error: {}", e),
-            Ok(m) => {
-                let payload = match m.payload_view::<str>() {
+            Err(e) => println!("Error consuming Event. Caused by: {}", e),
+            Ok(bm) => {
+                let _ = match bm.payload_view::<str>() {
                     None => "",
                     Some(Ok(s)) => s,
                     Some(Err(e)) => {
@@ -61,11 +71,19 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
                         ""
                     }
                 };
-                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-                         m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
-                consumer.commit_message(&m, CommitMode::Async).unwrap();
+                println!("key: '{:?}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                         bm.key(), bm.topic(), bm.partition(), bm.offset(), bm.timestamp());
+                commit_message(&consumer, &bm);
             }
         };
+    }
+}
+
+///Commit the message and control the side-effect of [KafkaResult]
+fn commit_message(consumer: &StreamConsumer<CustomContext>, bm: &BorrowedMessage) {
+    match consumer.commit_message(&bm, CommitMode::Async) {
+        Ok(_) => println!("Message commit successfully"),
+        Err(e) => println!("Error committing message. Caused by ${}", e.to_string()),
     }
 }
 
@@ -75,5 +93,5 @@ async fn main() {
     let brokers = "localhost:9092";
     let group_id = "red_panda_group-id";
 
-    consume_and_print(brokers, group_id, &topics).await
+    consume(brokers, group_id, &topics).await
 }
--- red_panda/src/red_panda_producer.rs
+++ red_panda/src/red_panda_producer.rs
@@ -1,44 +1,58 @@
+use std::fs;
+use std::future::Future;
 use std::time::Duration;
-
 use rdkafka::config::ClientConfig;
-use rdkafka::message::{OwnedHeaders};
 use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::util::get_rdkafka_version;
-
+use rdkafka::producer::future_producer::OwnedDeliveryResult;
 
 async fn produce(brokers: &str, topic_name: &str) {
-    let producer: &FutureProducer = &ClientConfig::new()
-        .set("bootstrap.servers", brokers)
-        .set("message.timeout.ms", "5000")
-        .create()
-        .expect("Producer creation error");
-
+    let producer = &create_producer(brokers);
     // This loop is non blocking: all messages will be sent one after the other, without waiting
     // for the results.
     let futures = (0..5)
         .map(|i| async move {
-            // The send operation on the topic returns a future, which will be
-            // completed once the result or failure from Kafka is received.
-            let delivery_status = producer
-                .send(
-                    FutureRecord::to(topic_name)
-                        .payload(&format!("Message {}", i))
-                        .key(&format!("Key {}", i)),
-                    Duration::from_secs(0),
-                )
-                .await;
-
-            // This will be executed when the result is received.
+            let delivery_result = send_record(topic_name, &producer, &i).await;
             println!("Delivery status for message {} received", i);
-            delivery_status
+            delivery_result
         })
         .collect::<Vec<_>>();
 
-    // This loop will wait until all delivery statuses have been received.
+    process_results(futures).await;
+}
+
+/// We create a producer using [ClientConfig] builder, where we set several keys as properties
+///  like server to connect, and timeout of the sent messages.
+/// We use [create] to create a [KafkaResult], that control side-effect trying to connect to Kafka broker.
+/// We use [expect] to try to get the [OK] value from the [KafkaResult], and otherwise print a specific error message in case of Panic.
+fn create_producer(brokers: &str) -> FutureProducer {
+    ClientConfig::new()
+        .set("bootstrap.servers", brokers)
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Red Panda Producer creation error")
+}
+
+
+/// Having a [FutureProducer] we use [send] operator to pass a [FutureRecord] together with Duration [Timeout]
+/// If we set timeout with 0 value it will wait forever.
+/// Since the scope of the [send] is async by design we need to create the FutureRecord inside the invocation.
+///
+async fn send_record(topic_name: &str, producer: &FutureProducer, i: &i32) -> OwnedDeliveryResult {
+    let body = &fs::read_to_string("resources/uuid.txt").unwrap();
+    let key = &format!("Key {}", i);
+    let record = FutureRecord::to(topic_name)
+        .payload(body)
+        .key(key);
+    producer.send(record, Duration::from_secs(0)).await
+}
+
+/// This loop will wait until all delivery statuses have been received.
+async fn process_results(futures: Vec<impl Future<Output=OwnedDeliveryResult> + Sized>) {
     for future in futures {
         println!("Future completed. Result: {:?}", future.await);
     }
 }
+
 
 #[tokio::main]
 async fn main() {
