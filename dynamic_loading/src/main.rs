886beba338987715af7bea0a3493294983d4d279
Pablo Pérez García <politrons@gmail.com>
2023-06-09 14:31:31 +0200

    Add dynamic loading

--- dynamic_loading/src/main.rs
+++ dynamic_loading/src/main.rs
@@ -3,18 +3,21 @@ extern crate libloading;
 use libloading::{Library, Symbol};
 use dynamic_loading_contract::PluginTrait;
 
+/// Using [libloading] library we're able to load Trait implementations in runtime, without have to know the specific type
+/// that implement the trait, allowing same pattern than in Java Service Provider Interface(SPI).
+/// This allow inject in a program different implementations just adding the path of the [.dylib] file that contains
+/// the implementation, and knowing the name of the [symbol] created in that library.
+///
+/// [Library::new] oad the shared object file
+/// [lib.get(b"your_symbol_name\0")] Use a symbol from the shared object file and return a [Symbol] function type
+/// [plugin_symbol_func()] we run the function so we get the dynamic object Box<dyn PluginTrait>
+/// [plugin.hello_world()] we run the action
+///  The shared object file is unloaded automatically when 'lib' goes out of scope
 fn main() {
     unsafe {
-        // Load the shared object file
         let lib = Library::new("../dynamic_loading_plugin/target/release/libdynamic_loading_plugin.dylib").unwrap();
-
-        // Use a symbol from the shared object file
-        let create_wrapper: Symbol<extern "C" fn() -> Box<dyn PluginTrait>> = lib.get(b"create_trait_wrapper\0").unwrap();
-        // Convert the symbol to a function pointer
-        let wrapper = create_wrapper();
-        // Run the action
-        wrapper.perform_action();
+        let plugin_symbol_func: Symbol<extern "C" fn() -> Box<dyn PluginTrait>> = lib.get(b"create_plugin\0").unwrap();
+        let plugin = plugin_symbol_func();
+        plugin.hello_world();
     }
-
-    // The shared object file is unloaded automatically when 'lib' goes out of scope
 }
--- dynamic_loading_contract/src/lib.rs
+++ dynamic_loading_contract/src/lib.rs
@@ -1,6 +1,6 @@
 ///The trait contract to be used by the [plugin] to create the implementation,
 /// and by the [dynamic_loading] cargo to load in runtime any implementation of this contract
 pub trait PluginTrait {
-    fn perform_action(&self);
+    fn hello_world(&self);
 }
 
