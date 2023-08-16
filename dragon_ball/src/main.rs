ab79e146b4c8f9eb84888165bf7b86615c266cb6
politrons <politrons@gmail.com>
2023-08-16 23:13:12 +0200

    new game engine

--- dragon_ball/src/main.rs
+++ dragon_ball/src/main.rs
@@ -99,7 +99,7 @@ fn setup(
     // let background_image = asset_server.load("namek.png");
 
 
-    let texture_handle = asset_server.load("gabe-idle-run.png");
+    let texture_handle = asset_server.load("vegeta.png");
     let texture_atlas =
         TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
