619f5fe520c8518b4aa830b59b343455e5311b60
politrons <politrons@gmail.com>
2023-08-16 18:31:41 +0200

    new game engine

--- dragon_ball/src/main.rs
+++ dragon_ball/src/main.rs
@@ -9,11 +9,20 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
         .add_systems(Startup, setup)
         .add_systems(Update, animate_sprite)
+        .add_systems(Update, animate_enemy)
+
         .run();
 }
 
 #[derive(Component)]
 struct AnimationIndices {
+    // running_right:bool,
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component)]
+struct EnemyIndices {
     // running_right:bool,
     first: usize,
     last: usize,
@@ -43,6 +52,7 @@ fn animate_sprite(
                 } else {
                     sprite.index + 1
                 };
+                // Move the sprite to the right
                 transform.translation.x += 10.0; // Adjust the movement speed as needed
             }
             if keyboard_input.pressed(KeyCode::Left) {
@@ -53,8 +63,30 @@ fn animate_sprite(
                 } else {
                     sprite.index + 1
                 };
+                // Move the sprite to the left
                 transform.translation.x -= 10.0; // Adjust the movement speed as needed
             }
+        }
+    }
+}
+
+fn animate_enemy(
+    time: Res<Time>,
+    mut query: Query<(
+        &EnemyIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        info!("'Enemy animation");
+        timer.tick(time.delta());
+        if timer.just_finished() {
+                sprite.index = if sprite.index == indices.last {
+                    indices.first
+                } else {
+                    sprite.index + 1
+                };
         }
     }
 }
@@ -65,12 +97,27 @@ fn setup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     // let background_image = asset_server.load("namek.png");
+
+
     let texture_handle = asset_server.load("gabe-idle-run.png");
     let texture_atlas =
         TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 1, last: 6 };
+
+
+    let texture_handle1 = asset_server.load("gabe-idle-run.png");
+    let texture_atlas1 =
+        TextureAtlas::from_grid(texture_handle1, Vec2::new(24.0, 24.0), 7, 1, None, None);
+    let texture_atlas_handle1 = texture_atlases.add(texture_atlas1);
+    let enemy_indices = EnemyIndices { first: 1, last: 6 };
+
+    // let texture_handle1 = asset_server.load("namek.png");
+    // let texture_atlas1 =
+    //     TextureAtlas::from_grid(texture_handle1, Vec2::new(24.0, 24.0), 7, 1, None, None);
+    // let texture_atlas_handle1 = texture_atlases.add(texture_atlas1);
+
+    // Use only the subset of sprites in the sheet that make up the run animation
     commands.spawn(Camera2dBundle::default());
     // commands.spawn(SpriteBundle {
     //     texture: background_image,
@@ -82,11 +129,26 @@ fn setup(
     commands.spawn((
         SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
-            sprite: TextureAtlasSprite::new(animation_indices.first),
+            sprite: TextureAtlasSprite::new(1),
             transform: Transform::from_scale(Vec3::splat(6.0)),
             ..default()
         },
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
     ));
+
+    let mut enemy_transform = Transform::default();
+    enemy_transform.scale=Vec3::splat(6.0);
+    enemy_transform.translation=Vec3::new(200.0,0.0,0.0);
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle1,
+            sprite: TextureAtlasSprite::new(1),
+            transform: enemy_transform,
+            ..default()
+        },
+        enemy_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+
 }
