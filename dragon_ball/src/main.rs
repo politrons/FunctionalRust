5d87901fd356ae8da025996f57d21cb869c9311f
politrons <politrons@gmail.com>
2023-08-19 23:42:51 +0200

    dragon ball game

--- dragon_ball/src/main.rs
+++ dragon_ball/src/main.rs
@@ -1,9 +1,12 @@
 //! Renders an animated sprite by loading all animation frames from a single image (a sprite sheet)
 //! into a texture atlas, and changing the displayed image periodically.
 
+use std::time::{Duration, SystemTime};
 use bevy::app::PluginGroupBuilder;
 use bevy::ecs::bundle::DynamicBundle;
+use bevy::ecs::schedule::NodeId::System;
 use bevy::prelude::*;
+use rand::Rng;
 use crate::DbzAction::{Blast, Fight, Hit, Ki, Move};
 use crate::GamePlayers::{Enemy, Player};
 
@@ -14,8 +17,8 @@ fn main() {
         .add_systems(Startup, setup_audio)
         .add_systems(Update, animate_player)
         .add_systems(Update, animate_enemy)
-        .insert_resource(GameInfo { enemy_action: Ki, player_action: Ki, turn: Player, action: Ki })
-    .run();
+        .insert_resource(GameInfo { turn_time: SystemTime::now(), enemy_action: Ki, player_action: Ki, turn: Player, action: Ki })
+        .run();
 }
 
 /// Setup of the App Window where using [WindowPlugin] we set the [Window] type with [title] and [resolution].
@@ -49,12 +52,13 @@ enum GamePlayers {
 #[derive(Resource)]
 struct GameInfo {
     turn: GamePlayers,
+    turn_time: SystemTime,
     player_action: DbzAction,
     enemy_action: DbzAction,
     action: DbzAction,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 enum DbzAction {
     Ki,
     Move,
@@ -107,14 +111,12 @@ fn animate_player(
         mut transform) in &mut query {
         timer.tick(time.delta());
         if timer.just_finished() {
-            info!("'GameInfo' turn {:?}", game_info.turn);
             transform.scale = Vec3::splat(0.0);
             match animation.entity {
                 Ki => {
                     let is_action_key = keyboard_input.pressed(KeyCode::Left) ||
                         keyboard_input.pressed(KeyCode::Space) ||
                         keyboard_input.pressed(KeyCode::Return);
-                    info!("'TrunkKi' currently pressed {}", is_action_key);
 
                     if !is_action_key {
                         sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
@@ -122,7 +124,6 @@ fn animate_player(
                     }
                 }
                 Move => if keyboard_input.pressed(KeyCode::Left) {
-                    info!("'TrunkMove' currently pressed");
                     sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
                     transform.scale = Vec3::splat(2.0);
                 },
@@ -131,14 +132,12 @@ fn animate_player(
                 }
                 Fight => {
                     if keyboard_input.pressed(KeyCode::Space) {
-                        info!("'TrunkFight' currently pressed");
                         sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
                         transform.scale = Vec3::splat(2.0);
                     }
                 }
                 Blast => {
                     if keyboard_input.pressed(KeyCode::Return) {
-                        info!("'TrunkBlast' currently pressed");
                         sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
                         transform.scale = Vec3::splat(2.0);
                     }
@@ -162,19 +161,36 @@ fn animate_enemy(
         mut timer,
         mut sprite,
         mut transform) in &mut query {
-        info!("'GameInfo' turn {:?}", game_info.turn);
         timer.tick(time.delta());
         if timer.just_finished() {
             transform.scale = Vec3::splat(0.0);
-            game_info.turn=Enemy;
-            match animation.entity {
-                Ki => {
-                    sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
-                    transform.scale = Vec3::splat(2.0);
+            game_info.turn = Enemy;
+            if game_info.turn_time.lt(&SystemTime::now()) {
+                game_info.action = throw_dice();
+                info!("Enemy action ${:?}", game_info.action);
+                game_info.turn_time = SystemTime::now() + Duration::from_secs(5);
+            }
+            if animation.entity == game_info.action {
+                sprite.index = move_sprite(animation.first, animation.last, &mut sprite);
+                transform.scale = Vec3::splat(2.0);
+                if animation.entity == Fight {
+                    transform.translation = Vec3::new(-240.0, 150.0, 1.0);
+                }else{
+                    transform.translation =  Vec3::new(300.0, 150.0, 1.0);
                 }
-                _ => {}
             }
         }
+    }
+}
+
+/// We throw
+fn throw_dice() -> DbzAction {
+    let mut rng = rand::thread_rng();
+    match rng.gen_range(0..10) {
+        1 | 2 => Ki,
+        3 | 6 => Fight,
+        7 | 8 => Blast,
+        _ => Move,
     }
 }
 
@@ -197,7 +213,6 @@ fn setup_sprites(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    // commands.insert_resource(info);
     commands.spawn(Camera2dBundle::default());
     setup_background(&mut commands, &asset_server, &mut texture_atlases);
     setup_player(&mut commands, &asset_server, &mut texture_atlases);
@@ -242,12 +257,28 @@ fn setup_enemy(mut commands: &mut Commands, asset_server: &Res<AssetServer>, mut
     let (enemy_ki_atlas_handle, ki_animation) =
         create_sprite(&asset_server, &mut texture_atlases, animation_func, Ki, "trunk_ki.png", 70.0, 60.0, 3, 1);
 
+    let (enemy_move_atlas_handle, move_animation) =
+        create_sprite(&asset_server, &mut texture_atlases, animation_func, Move, "trunk_move.png", 37.0, 55.0, 6, 1);
+
+    let (enemy_blast_atlas_handle, blast_animation) =
+        create_sprite(&asset_server, &mut texture_atlases, animation_func, Blast, "trunk_blast.png", 32.5, 49.0, 5, 1);
+
+    let (enemy_fight_atlas_handle, fight_animation) =
+        create_sprite(&asset_server, &mut texture_atlases, animation_func, Fight, "trunk_fight.png", 44.2, 42.0, 6, 1);
+
+    let (enemy_hit_atlas_handle, hit_animation) =
+        create_sprite(&asset_server, &mut texture_atlases, animation_func, Hit, "trunk_hit.png", 36.05, 52.0, 7, 1);
+
     let mut enemy_transform = Transform::default();
     enemy_transform.scale = Vec3::splat(2.0);
     enemy_transform.translation = Vec3::new(300.0, 150.0, 1.0);
     let mut sprite = TextureAtlasSprite::new(0);
     sprite.flip_x = true;
-    sprite_spawn(&mut commands, enemy_ki_atlas_handle, sprite, ki_animation, enemy_transform);
+    sprite_spawn(&mut commands, enemy_ki_atlas_handle, sprite.clone(), ki_animation, enemy_transform);
+    sprite_spawn(&mut commands, enemy_move_atlas_handle, sprite.clone(), move_animation, enemy_transform);
+    sprite_spawn(&mut commands, enemy_blast_atlas_handle, sprite.clone(), blast_animation, enemy_transform);
+    sprite_spawn(&mut commands, enemy_fight_atlas_handle, sprite.clone(), fight_animation, enemy_transform);
+    sprite_spawn(&mut commands, enemy_hit_atlas_handle, sprite.clone(), hit_animation, enemy_transform);
 }
 
 fn setup_background(mut commands: &mut Commands, asset_server: &Res<AssetServer>, mut texture_atlases: &mut ResMut<Assets<TextureAtlas>>) {
