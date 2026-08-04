307c166ae9f39bad542cb34ed86eb16f53ca4e21
Pablo Picouto García <politrons@gmail.com>
2026-08-04 11:22:46 +0200

    latest updates

--- src/features/promise.rs
+++ src/features/promise.rs
@@ -1,12 +1,8 @@
-use std::{thread, time};
-use std::future::Future;
-use std::io;
-use std::net::TcpListener;
-use std::sync::mpsc::{channel, Receiver, RecvError, SendError};
 use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, RecvTimeoutError, SendError};
 use std::sync::mpsc::Sender;
-
-use futures::executor::block_on;
+use std::{thread, time};
+use std::time::Duration;
 
 /// We can implement the [Promise] pattern in Rust using [Channels]
 /// We create a [Sender(Promise)] and the [Receiver(Future)].
@@ -37,17 +33,68 @@ async fn async_task(promise: Sender<String>) {
     }
 }
 
+// DSL promise
+// ------------
+struct Promise<T> {
+    sender: Sender<T>,
+    receiver: Receiver<T>,
+}
+
+struct PromiseCompleter<T> {
+    sender: Sender<T>,
+}
+
+trait PromiseI<T> {
+    fn promise() -> Promise<T>;
+    fn completer(&self) -> PromiseCompleter<T>;
+    fn future(&self, timeout: Duration) -> Result<T, RecvTimeoutError>;
+}
+
+impl<T> PromiseI<T> for Promise<T> {
+    fn promise() -> Promise<T> {
+        let (sender, receiver): (Sender<T>, Receiver<T>) = mpsc::channel();
+        Promise { sender, receiver }
+    }
+
+    fn completer(&self) -> PromiseCompleter<T> {
+        PromiseCompleter {
+            sender: self.sender.clone(),
+        }
+    }
+
+    fn future(&self, timeout: Duration) -> Result<T, RecvTimeoutError> {
+        self.receiver.recv_timeout(timeout)
+    }
+}
+
+impl<T> PromiseCompleter<T> {
+    fn succeed(&self, value: T) -> Result<(), SendError<T>> {
+        self.sender.send(value)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn promise_dsl() {
+        let promise: Promise<String> = Promise::promise();
+        let completer = promise.completer();
+
+        async_std::task::spawn(async move{
+            async_std::task::sleep(time::Duration::from_secs(2)).await;
+            completer.succeed(String::from("test")).unwrap();
+        });
+
+        match promise.future(Duration::from_secs(5)) {
+            Ok(value) => {println!("message received: {value}")}
+            Err(error) => {println!("error {}", error.to_string())}
+        }
+    }
 
     #[test]
     fn promise() {
         promise_feature()
     }
 }
-
-
-
-
-
